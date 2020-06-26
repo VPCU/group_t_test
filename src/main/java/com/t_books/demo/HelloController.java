5f0933b0038209696212531e9ddd3b52de1e0f0d
mhr <hzi@upc.com>
2020-06-26 12:43:29 +0800

    first commit

---
+++ src/main/java/com/t_books/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.t_books.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String IndexPage() {
+        return "<h1>It works!</h1>";
+    }
+}
